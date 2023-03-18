b6da1348b86919612df0f4eaa08f357c226c5915
ArchyZheng <zhen0144@e.ntu.edu.sg>
2023-03-18 13:41:09 +0800

    put y_hat and y into device

--- main.py
+++ main.py
@@ -9,27 +9,29 @@ from model import baseResnet
 import wandb
 import yaml
 
+
 def main():
     wandb.init()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    batch_size = wandb.config.batch_size # TODO: changed by wandb
+    batch_size = wandb.config.batch_size  # TODO: changed by wandb
     train_dataloader = get_train_or_val_dataloader(data_file='./FashionDataset', picture_list_dir='split/train.txt',
-                                                   label_list_dir='split/train_attr.txt', shuffle=True, batch_size=batch_size)
+                                                   label_list_dir='split/train_attr.txt', shuffle=True,
+                                                   batch_size=batch_size)
 
     val_dataloader = get_train_or_val_dataloader(data_file='./FashionDataset', picture_list_dir='split/val.txt',
-                                                   label_list_dir='split/val_attr.txt', shuffle=True, batch_size=batch_size)
+                                                 label_list_dir='split/val_attr.txt', shuffle=True,
+                                                 batch_size=batch_size)
     transform = nn.Sequential(
         T.Resize((224, 224))
     ).to(device)
     # model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
     model = baseResnet()
     model.to(device)
-    epochs = wandb.config.epochs # TODO: changed by wandb
+    epochs = wandb.config.epochs  # TODO: changed by wandb
     # TODO: optimizer dict
     optimizer = SGD(params=model.parameters(), lr=wandb.config.lr, momentum=0.9, weight_decay=wandb.config.wd)
     # TODO: scheduler dict
     scheduler = lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=epochs)
-
 
     criterion = nn.MultiLabelMarginLoss().to(device)
     for epoch in range(epochs):
@@ -49,16 +51,18 @@ def main():
                 input.append(img_tensor)
             input = torch.cat(input, 0).reshape(-1, 3, 224, 224).to(device)
             y_hat = model(input)
-            loss = criterion(y_hat, y)
+            loss = criterion(y_hat.to(device), y.to(device))
 
             loss.backward()
             optimizer.step()
             train_loss += batch_size * loss.item()
         wandb.log({'train_loss': train_loss})
 
+
 if __name__ == "__main__":
     import argparse
     import yaml
+
     parser = argparse.ArgumentParser(description='parameters')
     parser.add_argument('--cfg', type=str, default='config.yaml')
     parser.add_argument('--prj_name', type=str)
