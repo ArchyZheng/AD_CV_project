fb97435ef4b6ba4ae85ba6b47a11fb938aa8bffc
ArchyZheng <zhen0144@e.ntu.edu.sg>
2023-03-18 13:18:20 +0800

    available version

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ from torch.optim import SGD, lr_scheduler
 from model import baseResnet
 import wandb
 import yaml
+
 def main():
+    wandb.init()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    batch_size = 16 # TODO: changed by wandb
+    batch_size = wandb.config.batch_size # TODO: changed by wandb
     train_dataloader = get_train_or_val_dataloader(data_file='./FashionDataset', picture_list_dir='split/train.txt',
                                                    label_list_dir='split/train_attr.txt', shuffle=True, batch_size=batch_size)
 
@@ -22,9 +24,9 @@ def main():
     # model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
     model = baseResnet()
     model.to(device)
-    epochs = 200 # TODO: changed by wandb
+    epochs = wandb.config.epochs # TODO: changed by wandb
     # TODO: optimizer dict
-    optimizer = SGD(params=model.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
+    optimizer = SGD(params=model.parameters(), lr=wandb.config.lr, momentum=0.9, weight_decay=wandb.config.wd)
     # TODO: scheduler dict
     scheduler = lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=epochs)
 
@@ -52,17 +54,17 @@ def main():
             loss.backward()
             optimizer.step()
             train_loss += batch_size * loss.item()
-
-        # for X, Y in val_dataloader:
-        #     model.eval()
-        #     input = []
-        #     for i in range(len(X)):
-        #         img_tensor = read_image(X[i]).float().to(device)
-        #         img_tensor = transform(img_tensor)
-        #         input.append(img_tensor)
-        #
-        # break
-        #     # TODO: get loss
+        wandb.log({'train_loss': train_loss})
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    import yaml
+    parser = argparse.ArgumentParser(description='parameters')
+    parser.add_argument('--cfg', type=str, default='config.yaml')
+    parser.add_argument('--prj_name', type=str)
+    args = parser.parse_args()
+
+    with open(args.cfg, 'r') as stream:
+        sweep_configuration = yaml.safe_load(stream)
+    sweep_id = wandb.sweep(sweep=sweep_configuration, project=args.prj_name)
+    wandb.agent(sweep_id=sweep_id, function=main)
