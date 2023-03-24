3f0701b1b4b6077de30cf6b250cedf342b68ce3f
ArchyZheng <zhen0144@e.ntu.edu.sg>
2023-03-24 17:56:33 +0800

    add autoaugmentation

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ def train(model, dataloader, optimiser, criterion, transforms, common):
     for x, y in dataloader:
         optimiser.zero_grad()
         x = x.to(device)
-        color = T.RandomApply(nn.Sequential(
-                              T.ColorJitter(brightness=.5, hue=.3)), p=0.5)
-        x = color(x)
+        # color = T.RandomApply(nn.Sequential(
+        #                       T.ColorJitter(brightness=.5, hue=.3)), p=0.5)
+        # x = color(x)
         x = common(x)
         x = transforms(x)
         y = y.to(device)
