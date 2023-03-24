2190907f04f95fd7ba0c306291631f0d2d5384f2
ArchyZheng <zhen0144@e.ntu.edu.sg>
2023-03-24 18:25:54 +0800

    tial

--- main.py
+++ main.py
@@ -107,8 +107,8 @@ def main():
     ), p=wandb.config.trans_probability).to(device)
 
     transforms_test = nn.Sequential(
-        # T.Normalize(mean=[195.333, 187.721, 185.050], std=[72.6773, 75.9105, 77.4861]),
-        T.Grayscale()
+        T.Normalize(mean=[195.333, 187.721, 185.050], std=[72.6773, 75.9105, 77.4861]),
+        T.Grayscale(num_output_channels=3)
     ).to(device)
 
     for epoch in range(epochs):
