9b1c52de1065f78fe01ea7fa86a5de09242eda90
ArchyZheng <zhen0144@e.ntu.edu.sg>
2023-03-24 09:04:49 +0800

    fix test

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 from dataset_test import fashionDataset_1
 import torch
 from model import baseResnet
@@ -6,14 +6,16 @@ from torch.utils.data import DataLoader
 from utlis import formal_output
 import numpy as np
 import tqdm
-#%%
-def test(config, model_dir):
+
+
+# %%
+def test(config, model_base, model_name):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = baseResnet(baseModel=config.baseModel)
+    model = baseResnet(baseModel=model_base)
     test_dataset = fashionDataset_1('test_tensor.pt')
     test_dataloader = DataLoader(dataset=test_dataset, shuffle=False, batch_size=100)
     # model.load_state_dict(torch.load('model.pt', map_location=('cpu')))
-    model.load_state_dict(torch.load('model.pt'))
+    model.load_state_dict(torch.load(f'model-{model_name}.pt'))
     index_1 = []
     count = 0
     for X in test_dataloader:
@@ -22,4 +24,7 @@ def test(config, model_dir):
         index_1.append(index)
     index = torch.cat(index_1, dim=0)
     index = index.numpy()
-    np.savetxt("prediction.txt", index, fmt="%.d")
+    np.savetxt(f"prediction_{model_name}.txt", index, fmt="%.d")
+
+
+test(model_base="Resnet50", model_name='9b86h35x')
