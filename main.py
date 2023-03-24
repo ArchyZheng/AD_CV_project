c36b964956cbd843b02b91e8a58b9bbb57eb3ac5
ArchyZheng <zhen0144@e.ntu.edu.sg>
2023-03-24 10:09:47 +0800

    fix bug

--- main.py
+++ main.py
@@ -92,9 +92,9 @@ def main():
     criterion = nn.BCEWithLogitsLoss(weight=weight).to(device)
 
     transforms_train = nn.Sequential(
-        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         T.RandomResizedCrop(size=(112, 112)),
-        T.Resize(225, 225)
+        T.Resize(224, 224),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         # T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))
     ).to(device)
     transforms_test = nn.Sequential(
